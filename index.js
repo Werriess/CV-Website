45d264d0870a3840ea7aa96262f4fa9dfc76e0f3
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-05-19 23:27:30 +0200

    Add express server and contact page

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express"
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import { join } from 'path';
+import { dirname } from "path";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.use('/css', express.static(join(__dirname, 'css')));
+
+app.use('/images', express.static(join(__dirname, 'images')));
+
+app.use('/files', express.static(join(__dirname, 'files')));
+
+app.use('/CVWebsiteFav.ico', express.static(join(__dirname, 'CVWebsiteFav.ico')));
+
+app.get("/contact", (req, res) => {
+    res.sendFile(join(__dirname, 'contact.html'))
+})
+
+app.post('/submit', (req, res) => {
+    const { fullName, email, phone, message } = req.body;
+    console.log(`Full Name: ${fullName}, Email: ${email}, Phone: ${phone}, Message: ${message}`);
+    res.send('Form data received and processed.');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`)
+})
