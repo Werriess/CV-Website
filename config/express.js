3b11ae4c51878c1329b4e29f1685e2ad90d8d64e
Werner Janse van Rensburg <wernerjvanrensburg15@gmail.com>
2024-08-26 17:09:06 +0200

    Add skills animation

--- config/express.js
+++ config/express.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
-import usersRouter from '../routes/index.js'; 
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+import usersRouter from "../routes/index.js";
 
 const app = express();
 
@@ -11,35 +11,27 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/css", express.static(join(__dirname, "../public/css")));
+app.use("/images", express.static(join(__dirname, "../public/images")));
+app.use("/uploads", express.static(join(__dirname, "../public/uploads")));
+app.use("/js", express.static(join(__dirname, "../public/js")));
+
 app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, '../views/index.html'));
-});
-
-app.use('/css', express.static(join(__dirname, '../public/css')));
-app.use('/users/css', express.static(join(__dirname, '../public/css')));
-app.use('/users/images', express.static(join(__dirname, '../public/images')));
-app.use('/images', express.static(join(__dirname, '../public/images')));
-app.use('/uploads', express.static(join(__dirname, '../public/uploads')));
-app.use('/js', express.static(join(__dirname, '../public/js')));
-app.use('/users/js', express.static(join(__dirname, '../public/js')));
-app.use('/users', usersRouter);
-
-
-app.get("/home", (req, res) => {
-    res.sendFile(join(__dirname, '../views/index.html'));
+  res.sendFile(join(__dirname, "../views/index.html"));
 });
 
 app.get("/home", (req, res) => {
-    res.sendFile(join(__dirname, '../views/index.html'));
+  res.sendFile(join(__dirname, "../views/index.html"));
 });
 
 app.get("/contact", (req, res) => {
-    res.sendFile(join(__dirname, '../views/contact.html'));
+  res.sendFile(join(__dirname, "../views/contact.html"));
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(join(__dirname, '../views/about.html'));
+  res.sendFile(join(__dirname, "../views/about.html"));
 });
 
+app.use("/users", usersRouter);
 
 export default app;
